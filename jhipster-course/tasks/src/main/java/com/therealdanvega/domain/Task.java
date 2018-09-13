10a082372c8adbf93509c3f744e67b053e709ae4
Evangelos Konstantinidis <evangelos.konstantinidis@exxeta.com>
2018-09-13 17:09:57 +0200

    first commit

---
+++ jhipster-course/tasks/src/main/java/com/therealdanvega/domain/Task.java
@@ -0,0 +1,35 @@
+package com.therealdanvega.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate dueDate;
+    private Boolean completed;
+
+    public Task() {
+
+    }
+
+}
